7a63c43f31c4ea9092cb29f24eccbe77441d3a69
Ndagi Stanley <ndagis@gmail.com>
2022-10-06 17:42:51 +0300

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,11 @@
 import {
   render,
   screen,
+  waitForElementToBeRemoved,
+  fireEvent
 } from '@testing-library/react';
 import '@testing-library/jest-dom'
+import userEvent from "@testing-library/user-event";
 import App from './App';
 import { makeServer } from './server'
 
@@ -21,7 +24,25 @@ afterEach(() => {
 test('Page loads successfully', async () => {
   render(<App />);
 
-  expect(screen.getByText("Loading...")).toBeInTheDocument()
+  await waitForElementToBeRemoved(() => screen.getByText("Loading..."))
   expect(screen.getByText("Todos")).toBeInTheDocument()
 
 });
+
+test('Initial todos are displayed', async () => {
+  server.create("todo", { name: "Grooming the cat" })
+  render(<App />);
+  await waitForElementToBeRemoved(() => screen.getByText("Loading..."))
+
+  expect(screen.getByText("Grooming the cat")).toBeInTheDocument()
+
+})
+
+test('Todo can be created', async () => {
+  render(<App />);
+  await waitForElementToBeRemoved(() => screen.getByText("Loading..."))
+
+  const postTodo = await screen.findByTestId("post-todo")
+  userEvent.type(postTodo.querySelector("input[type=text]"), "Feed the cat")
+  fireEvent.submit(screen.getByTestId("post-todo"))
+})
