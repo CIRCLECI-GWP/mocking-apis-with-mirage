ab8f2a2b880a01c2528a7a630897d2f694f51b5c
Ndagi Stanley <ndagis@gmail.com>
2022-10-06 17:11:49 +0300

    Static GET Handler

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import { Server } from 'miragejs';
 
+let todos = [
+    { id: 1, name: "Groom the cat" },
+    { id: 2, name: "Do the dishes" },
+    { id: 3, name: "Go shopping" }
+]
+
 export function makeServer() {
     let server = new Server({
         routes() {
             // GET REQUEST
             this.get("/api/todos", () => {
-                return {}
+                return {
+                    todos
+                }
             })
         }
     })
